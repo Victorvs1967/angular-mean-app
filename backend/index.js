4aa36bfb5f77f675b789ffd27ff2078d9ab07d34
Victors1967 <victorsmirnov@me.com>
2021-10-11 11:13:16 +0300

    1111

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('./db.js');
 const routes = require('./routes/routes.js');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors({origin: 'http://localhost:4200'}));
 
 app.listen(3000, () => console.log('Server started on port: 3000'));
