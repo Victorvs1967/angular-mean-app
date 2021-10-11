676dd9b21df059c6c9127a2847299367dd32d255
Victors1967 <victorsmirnov@me.com>
2021-10-11 13:08:03 +0300

    add link to backend

--- frontend/src/app/components/employee/employee.component.ts
+++ frontend/src/app/components/employee/employee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -8,14 +10,40 @@ import { Component, OnInit } from '@angular/core';
 export class EmployeeComponent implements OnInit {
 
   showModal = false;
+  editMode = false;
+  empForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.empForm = this.fb.group({
+      _id: [''],
+      name: ['Ex. Victor Smirnov', Validators.required],
+      position: ['Ex. Full Stack Development', Validators.required],
+      dept: ['Development']
+    });
   }
 
-  onAddEmployee = () => {};
+  onEmpSubmit = () => {
+    if (this.empForm.valid) {
+      this.employeeService.addEmployee(this.empForm.value).subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      );
+      if (this.editMode) {} else {}
+    } else {
+      let key = Object.keys(this.empForm.controls);
+      key.filter(data => {
+        let control = this.empForm.controls[data];
+        if (control.errors != null) {
+          control.markAsTouched();
+        }
+      })
+    }
+  };
 
-  onCloseModal = () => {};
+  onAddEmployee = () => this.showModal = true;
+
+  onCloseModal = () => this.showModal = false;
 
 }
---
+++ frontend/src/app/services/employee.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../models/employee.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http: HttpClient) { }
+
+  addEmployee = (emp: Employee) => this.http.post(environment.backUrl, emp);
+}
